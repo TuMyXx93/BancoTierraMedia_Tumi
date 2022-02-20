c051da7ad1da9e2ad6d967d44d63511bd584c8ef
TuMyXx93 <tumyxx@gmail.com>
2022-02-19 21:33:07 -0500

    Agregando clase Inicio

--- BancoTierraMedia_Tumi/Inicio.Designer.cs
+++ BancoTierraMedia_Tumi/Inicio.Designer.cs
@@ -29,18 +29,88 @@ namespace BancoTierraMedia_Tumi
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnSolicitarPrestamo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 175);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(118, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(425, 39);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Banco de la Tierra Media";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BancoTierraMedia_Tumi.Properties.Resources.book;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(187, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Su nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(325, 183);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(218, 22);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnSolicitarPrestamo
+            // 
+            this.btnSolicitarPrestamo.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnSolicitarPrestamo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolicitarPrestamo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSolicitarPrestamo.Location = new System.Drawing.Point(325, 244);
+            this.btnSolicitarPrestamo.Name = "btnSolicitarPrestamo";
+            this.btnSolicitarPrestamo.Size = new System.Drawing.Size(116, 58);
+            this.btnSolicitarPrestamo.TabIndex = 4;
+            this.btnSolicitarPrestamo.Text = "Solicitar Préstamo";
+            this.btnSolicitarPrestamo.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(684, 397);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(108, 52);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // Inicio
             // 
@@ -48,6 +118,11 @@ namespace BancoTierraMedia_Tumi
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Peru;
             this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnSolicitarPrestamo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -55,6 +130,9 @@ namespace BancoTierraMedia_Tumi
             this.Name = "Inicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Banco Tierra Media";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,6 +141,12 @@ namespace BancoTierraMedia_Tumi
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnSolicitarPrestamo;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- BancoTierraMedia_Tumi/Inicio.cs
+++ BancoTierraMedia_Tumi/Inicio.cs
@@ -16,5 +16,42 @@ namespace BancoTierraMedia_Tumi
         {
             InitializeComponent();
         }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            btnSolicitarPrestamo.Enabled = false;
+        }
+
+        private void controlBotones()
+        {
+            if (txtNombre.Text.Trim() != string.Empty && txtNombre.Text.All(Char.IsLetter))
+            {
+                btnSolicitarPrestamo.Enabled = true;
+                errorProvider1.SetError(txtNombre, "");
+            }
+            else
+            {
+                if (!(txtNombre.Text.All(Char.IsLetter)))
+                {
+                    errorProvider1.SetError(txtNombre, "El nombre sólo debe contener letras");
+                }
+                else
+                {
+                    errorProvider1.SetError(txtNombre, "Debe introducir su nombre");
+                }
+                btnSolicitarPrestamo.Enabled = false;
+                txtNombre.Focus();
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            controlBotones();
+        }
     }
 }
